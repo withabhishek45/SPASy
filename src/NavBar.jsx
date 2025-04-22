62dd09fa69ab9f3cd0336b9d1a49c4f71a9946f1
withabhishek45 <abhishekkumar28102005@gmail.com>
2025-04-22 20:11:15 +0530

    installed react router dom

---
+++ src/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+//rafce
+
+const NavBar = () => {
+  return (
+    <>
+    <div className="navbar bg-blue-400 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Smart Placement Assistant Sysytem</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal items-center px-1">
+      <li><a>Login</a></li>
+      <li>
+      <details className="dropdown">
+  <summary className="btn m-1 bg-accent">Register Here</summary>
+  <ul className="menu dropdown-content bg-black-800 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a>Student</a></li>
+    <li><a>Recuiter</a></li>
+  </ul>
+</details>
+      </li>
+    </ul>
+  </div>
+</div>
+    </>
+    
+  )
+}
+
+export default NavBar;
