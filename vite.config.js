5bbab6f61d1aadfe15be6b94b687a9a38a075193
withabhishek45 <abhishekkumar28102005@gmail.com>
2025-04-22 20:05:39 +0530

    builed a nav bar by tailwind css & daisyui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://vitejs.dev/guide/features.html#css
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
